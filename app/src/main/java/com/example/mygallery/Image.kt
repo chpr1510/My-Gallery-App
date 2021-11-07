a4c90663b90c3c1bf177249e37a52f46a16c3e1a
Dice-roller <chinni.pr003@gmail.com>
2021-11-07 10:57:28 +0530

    MVC architecture started

--- app/src/main/java/com/example/mygallery/Image.kt
+++ app/src/main/java/com/example/mygallery/Image.kt
@@ -1,4 +1,11 @@
 package com.example.mygallery
 
 class Image {
+    var imagePath:String?=null
+    var imageNmae:String?=null
+
+    constructor(imagePath: String?, imageNmae: String?) {
+        this.imagePath = imagePath
+        this.imageNmae = imageNmae
+    }
 }
--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.mygallery
 
 import android.os.Bundle
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private var imageRecycler:RecyclerView?=null
+    private var Progressbar:ProgressBar?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageRecycler = findViewById(R.id.image_recycler)
+        Progressbar = findViewById(R.id.RecyclerProgress)
     }
 }
